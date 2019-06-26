2edfe35e5acd4cb4125acf360346bfbaa88841ac
Srinivasa Rao Nagalla (DAAI - Cloud Data Platforms) <SR249284@wipro.com>
2019-06-26 22:48:57 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Navbar from './Components/Navbar/Navbar'
+import { Route } from 'react-router-dom';
+import Inbox from './Components/Inbox/Inbox';
+import Outbox from './Components/Outbox/Outbox';
+import User from './Components/User/User';
 
-function App() {
+const  App=()=> {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar>
+   
+      <Route path='/signup'  component={Inbox} />
+        <Route path='/signin' component={Outbox} />
+        <Route path='/UserHome' component={User} />
+        </Navbar>
     </div>
   );
 }
---
+++ src/Components/User/User.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const User = (props) =>(
+    <div>
+        <h1>Hiii User {props.email}XXXXXXXXX</h1>
+    </div>
+);
+
+export default User;
